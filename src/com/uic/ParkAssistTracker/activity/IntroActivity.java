e67a29a45c2b908356aba1ca87242c14e05cf3da
unknown <amahaj3@uic.edu>
2014-04-27 22:26:47 -0500

    Geofencing integration

--- src/com/uic/ParkAssistTracker/activity/IntroActivity.java
+++ src/com/uic/ParkAssistTracker/activity/IntroActivity.java
@@ -1,6 +1,8 @@
 package com.uic.ParkAssistTracker.activity;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -34,5 +36,7 @@ public class IntroActivity extends Activity {
 
 
     }
+
+
 
 }
